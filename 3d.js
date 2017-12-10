296af2cdeed784e3f3e1a537a2dccb9c4bb562df
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2017-12-10 15:56:08 -0500

    Create 3d.js

---
+++ 3d.js
@@ -0,0 +1,17 @@
+var x;
+var y;
+var z;
+var 3d={
+setX: function(param) {
+x=param;
+},
+setY: function(param) {
+y=param;
+},
+setZ: function(param) {
+z=param;
+},
+xrot: function(rotation) {
+//WIP
+}
+};
